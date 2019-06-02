2e1a7ce526b142795c8854935db2f5b76b454706
Arthur Song <arth3rs0ng@gmail.com>
2019-06-02 22:07:45 +0000

    added create tables to migration files

--- db/migrate/20190602220216_create_video_posts.rb
+++ db/migrate/20190602220216_create_video_posts.rb
@@ -1,4 +1,9 @@
 class CreateVideoPosts < ActiveRecord::Migration
   def change
+    create_table :video_posts do |t|
+      t.string :title
+      t.string :video_link
+      t.integer :creator_id
+    end
   end
 end
--- db/migrate/20190602220231_create_user_video_posts.rb
+++ db/migrate/20190602220231_create_user_video_posts.rb
@@ -1,4 +1,8 @@
 class CreateUserVideoPosts < ActiveRecord::Migration
   def change
+    create_table :user_video_posts do |t|
+      t.integer :user_id
+      t.integer :video_post_id
+    end
   end
 end
