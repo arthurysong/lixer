b972c192e9d143afc69911a51fa9015dcd713d08
Arthur Song <arth3rs0ng@gmail.com>
2019-06-02 22:16:45 +0000

    created association creator and created_video_posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   
-  
+  has_many :created_video_posts,
+           :class_name => "VideoPost",
+           :foreign_key => :creator_id
 end
--- app/models/video_post.rb
+++ app/models/video_post.rb
@@ -1,3 +1,5 @@
 class VideoPost < ActiveRecord::Base
-  
+  belongs_to :creator, 
+             :class_name => "User", 
+             :foreign_key => :creator_id
 end
