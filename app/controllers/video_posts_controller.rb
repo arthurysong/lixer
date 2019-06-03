5844519e543f6d4a140991b86b1d395873e5e54a
Arthur Song <arth3rs0ng@gmail.com>
2019-06-03 00:07:03 +0000

    route for post method for creating video post

--- app/controllers/video_posts_controller.rb
+++ app/controllers/video_posts_controller.rb
@@ -9,9 +9,24 @@ class VideoPostsController < ApplicationController
   
   get '/video-posts/new' do
     if logged_in?
-      erb :'video-posts/new'
+      erb :'video_posts/new'
     else
       redirect '/login'
     end
   end
+  
+  post '/video-posts' do
+    if params["title"] == "" || params["video_link"] == ""
+      flash[:message] = "Please enter a title and video link."
+      redirect '/video-posts/new'
+    end
+    
+    user = current_user
+    videopost = VideoPost.create(params)
+    videopost.creator = user
+    videopost.save
+    redirect "/video-posts/#{videopost.id}"
+  end
+  
+  
 end
